5e43ada45dd2a455ab1b285940399ad1c7cde926
Derek Jass <jazzer36@gmail.com>
2013-10-31 14:06:55 -0500

    Work work.

--- Scoresheet/src/drj/scoresheet/AveragePickerFragment.java
+++ Scoresheet/src/drj/scoresheet/AveragePickerFragment.java
@@ -1,7 +1,64 @@
 package drj.scoresheet;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AveragePickerFragment extends DialogFragment {
 
+	private Button selected = null;
+
+	private OnClickListener buttonListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			if (selected != null) {
+				selected.setPressed(false);
+			}
+			v.setPressed(true);
+			selected = (Button) v;
+		}
+	};
+
+	public interface Listener {
+		public void onAveragePicked(DialogFragment dialog);
+	}
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		LayoutInflater inflater = getActivity().getLayoutInflater();
+
+		View v = inflater.inflate(R.layout.dialog_average, null);
+		setupListeners(v);
+		TextView name = (TextView) v.findViewById(R.id.dialog_name);
+		name.setText(getArguments().getString("name"));
+
+		builder.setTitle(R.string.assign_average)
+		.setView(v);
+
+
+		return builder.create();
+	}
+
+	private void setupListeners(View v) {
+		LinearLayout buttons =
+				(LinearLayout) v.findViewById(R.id.dialog_ave_buttons1);
+
+		for (int i = 0; i < buttons.getChildCount(); i++) {
+			buttons.getChildAt(0).setOnClickListener(buttonListener);
+		}
+
+		buttons = (LinearLayout) v.findViewById(R.id.dialog_ave_buttons2);
+
+		for (int i = 0; i < buttons.getChildCount(); i++) {
+			buttons.getChildAt(0).setOnClickListener(buttonListener);
+		}
+	}
 }
--- Scoresheet/src/drj/scoresheet/ScoresheetActivity.java
+++ Scoresheet/src/drj/scoresheet/ScoresheetActivity.java
@@ -5,6 +5,8 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ScoresheetActivity extends Activity {
 
@@ -12,6 +14,8 @@ public class ScoresheetActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_scoresheet);
+
+		initializeLinks();
 	}
 
 	@Override
@@ -20,23 +24,59 @@ public class ScoresheetActivity extends Activity {
 		.setIcon(android.R.drawable.ic_dialog_alert)
 		.setTitle("Closing Activity")
 		.setMessage("Are you sure you want to close this activity? " +
-		"Doing so will clear the score boxes.")
-		.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				ScoresheetActivity.super.onBackPressed();
-			}
+				"Doing so will clear the score boxes.")
+				.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						ScoresheetActivity.super.onBackPressed();
+					}
 
-		})
-		.setNegativeButton("No", null)
-		.show();
+				})
+				.setNegativeButton("No", null)
+				.show();
 	}
 
 	public void onScoreBoxClicked(View v) {
-
+		TextView clickedBox = (TextView) v;
+		clickedBox.setText("10");
+		TextView otherBox = (TextView) clickedBox.getTag();
+		otherBox.setText("7");
 	}
 
 	public void onAverageBoxClicked(View v) {
+		Bundle args = new Bundle();
+		ViewGroup parent = (ViewGroup) v.getParent();
+		TextView name = (TextView) parent.getChildAt(1);
+		args.putString("name", name.getText().toString());
+		
+		AveragePickerFragment dialog = new AveragePickerFragment();
+		dialog.setArguments(args);
+		dialog.show(getFragmentManager(), "AverageDialog");
+	}
 
+	private void linkScoreBoxes(int homeResId, int awayResId) {
+		View home = findViewById(homeResId);
+		View away = findViewById(awayResId);
+
+		home.setTag(away);
+		away.setTag(home);
+	}
+
+	private void initializeLinks() {
+		linkScoreBoxes(R.id.homePlayer1Round1, R.id.awayPlayer1Round1);
+		linkScoreBoxes(R.id.homePlayer2Round1, R.id.awayPlayer2Round1);
+		linkScoreBoxes(R.id.homePlayer3Round1, R.id.awayPlayer3Round1);
+		linkScoreBoxes(R.id.homePlayer4Round1, R.id.awayPlayer4Round1);
+		linkScoreBoxes(R.id.homePlayer5Round1, R.id.awayPlayer5Round1);
+		linkScoreBoxes(R.id.homePlayer1Round2, R.id.awayPlayer2Round2);
+		linkScoreBoxes(R.id.homePlayer2Round2, R.id.awayPlayer4Round2);
+		linkScoreBoxes(R.id.homePlayer3Round2, R.id.awayPlayer1Round2);
+		linkScoreBoxes(R.id.homePlayer4Round2, R.id.awayPlayer5Round2);
+		linkScoreBoxes(R.id.homePlayer5Round2, R.id.awayPlayer3Round2);
+		linkScoreBoxes(R.id.homePlayer1Round3, R.id.awayPlayer3Round3);
+		linkScoreBoxes(R.id.homePlayer2Round3, R.id.awayPlayer1Round3);
+		linkScoreBoxes(R.id.homePlayer3Round3, R.id.awayPlayer5Round3);
+		linkScoreBoxes(R.id.homePlayer4Round3, R.id.awayPlayer2Round3);
+		linkScoreBoxes(R.id.homePlayer5Round3, R.id.awayPlayer4Round3);
 	}
 }
