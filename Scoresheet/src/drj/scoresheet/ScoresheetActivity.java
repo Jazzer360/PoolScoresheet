745bc19ea888a30a0e929dacbb12c0d56c797dd1
Derek Jass <jazzer36@gmail.com>
2013-10-30 22:44:12 -0500

    Finishing up id woes

--- Scoresheet/src/drj/scoresheet/ScoresheetActivity.java
+++ Scoresheet/src/drj/scoresheet/ScoresheetActivity.java
@@ -3,19 +3,14 @@ package drj.scoresheet;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class ScoresheetActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 		setContentView(R.layout.activity_scoresheet);
-	}
-	
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
 	}
 	
 	public void onScoreBoxClicked(View v) {
