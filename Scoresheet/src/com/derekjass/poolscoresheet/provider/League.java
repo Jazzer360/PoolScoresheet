74d116bae7f87d05d0d87c1545136d47c201e781
Derek Jass <jazzer36@gmail.com>
2013-11-14 15:43:31 -0600

    Content Provider work.

--- Scoresheet/src/com/derekjass/poolscoresheet/provider/League.java
+++ Scoresheet/src/com/derekjass/poolscoresheet/provider/League.java
@@ -14,16 +14,24 @@ public final class League {
 		private Match() {}
 
 		public static final String TABLE_NAME = "match";
-		public static final String COLUMN_NAME_DATE = "date";
-		public static final String COLUMN_HOME_TEAM = "hometeam";
-		public static final String COLUMN_AWAY_TEAM = "awayteam";
-		public static final String COLUMN_NAME_HOME_PLAYERS = "homeplayers";
-		public static final String COLUMN_NAME_AWAY_PLAYERS = "awayplayers";
-		public static final String COLUMN_AVE_HOME_PLAYERS = "homeaves";
-		public static final String COLUMN_AVE_AWAY_PLAYERS = "awayaves";
-		public static final String COLUMN_SCORES_HOME_PLAYERS = "homescores";
-		public static final String COLUMN_SCORES_AWAY_PLAYERS = "awayscores";
-		public static final String COLUMN_HOME_ERO_FLAGS = "homeeroflags";
-		public static final String COLUMN_AWAY_ERO_FLAGS = "awayeroflags";
+		public static final String COLUMN_DATE = "date";
+		public static final String COLUMN_TEAM_HOME = "hometeam";
+		public static final String COLUMN_TEAM_AWAY = "awayteam";
+		public static final String COLUMN_PLAYER_NAMES_HOME = "homeplayers";
+		public static final String COLUMN_PLAYER_NAMES_AWAY = "awayplayers";
+		public static final String COLUMN_PLAYER_AVES_HOME = "homeaves";
+		public static final String COLUMN_PLAYER_AVES_AWAY = "awayaves";
+		public static final String COLUMN_PLAYER_SCORES_HOME = "homescores";
+		public static final String COLUMN_PLAYER_SCORES_AWAY = "awayscores";
+		public static final String COLUMN_ERO_BITMASK = "erobitmask";
+		public static final String COLUMN_ROUND_WINS_HOME = "homeroundwins";
+		public static final String COLUMN_ROUND_WINS_AWAY = "awayroundwins";
+
+		public static final String CONTENT_TYPE =
+				"vnd.android.cursor.dir/vnd.com.derekjass.provider." +
+						TABLE_NAME;
+		public static final String CONTENT_ITEM_TYPE =
+				"vnd.android.cursor.item/vnd.com.derekjass.provider." +
+						TABLE_NAME;
 	}
 }
---
+++ Scoresheet/src/com/derekjass/poolscoresheet/provider/LeagueProvider.java
@@ -0,0 +1,140 @@
+package com.derekjass.poolscoresheet.provider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import com.derekjass.poolscoresheet.provider.League.Match;
+
+public class LeagueProvider extends ContentProvider {
+
+	private static final class DatabaseHelper extends SQLiteOpenHelper {
+
+		private static final int DATABASE_VERSION = 1;
+		private static final String DATABASE_NAME = "League.db";
+
+		private static final String SQL_CREATE_MATCHES =
+				"CREATE TABLE" + Match.TABLE_NAME + " (" +
+						Match._ID + " INTEGER PRIMARY KEY," +
+						Match.COLUMN_DATE + " INTEGER," +
+						Match.COLUMN_TEAM_HOME + " TEXT," +
+						Match.COLUMN_TEAM_AWAY + " TEXT," +
+						Match.COLUMN_PLAYER_AVES_HOME + " TEXT," +
+						Match.COLUMN_PLAYER_AVES_AWAY + " TEXT," +
+						Match.COLUMN_PLAYER_NAMES_HOME + " TEXT," +
+						Match.COLUMN_PLAYER_NAMES_AWAY + " TEXT," +
+						Match.COLUMN_PLAYER_SCORES_HOME + " TEXT," +
+						Match.COLUMN_PLAYER_SCORES_AWAY + " TEXT," +
+						Match.COLUMN_ERO_BITMASK + " INTEGER," +
+						Match.COLUMN_ROUND_WINS_HOME + " INTEGER," +
+						Match.COLUMN_ROUND_WINS_AWAY + " INTEGER)";
+
+		private DatabaseHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL(SQL_CREATE_MATCHES);
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion,
+				int newVersion) {
+		}
+
+	}
+
+	private DatabaseHelper dbHelper;
+	private static UriMatcher uriMatcher;
+
+	private static final int URI_MATCHES = 1;
+	private static final int URI_MATCH_ID = 2;
+
+	static {
+		uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+		uriMatcher.addURI(League.AUTHORITY,
+				Match.TABLE_NAME, URI_MATCHES);
+		uriMatcher.addURI(League.AUTHORITY,
+				Match.TABLE_NAME + "/#", URI_MATCH_ID);
+	}
+
+	@Override
+	public boolean onCreate() {
+		dbHelper = new DatabaseHelper(getContext());
+		return true;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+		String matchSelection = null;
+		String orderBy = TextUtils.isEmpty(sortOrder) ?
+				Match.COLUMN_DATE + " DESC" : sortOrder;
+
+		switch (uriMatcher.match(uri)) {
+		case URI_MATCHES:
+			matchSelection = selection;
+			break;
+		case URI_MATCH_ID:
+			matchSelection = Match._ID + "=" + uri.getPathSegments().get(1);
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+
+		SQLiteDatabase db = dbHelper.getReadableDatabase();
+		return db.query(uri.getPathSegments().get(0),
+				projection,
+				matchSelection,
+				selectionArgs,
+				null,
+				null,
+				orderBy);
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		switch (uriMatcher.match(uri)) {
+		case URI_MATCHES:
+			return Match.CONTENT_TYPE;
+		case URI_MATCH_ID:
+			return Match.CONTENT_ITEM_TYPE;
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues initialValues) {
+		if (uriMatcher.match(uri) != URI_MATCHES) {
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+
+		ContentValues values = (initialValues != null ?
+				new ContentValues(initialValues) : new ContentValues());
+		
+		if (!values.containsKey(Match.COLUMN_DATE))
+			values.put(Match.COLUMN_DATE, System.currentTimeMillis());
+	}
+
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
